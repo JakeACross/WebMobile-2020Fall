0ee80a034bb551cc7d8bcf126b19af05846bf1be
JakeACross <jackgw@mail.umkc.edu>
2020-09-18 12:28:59 -0500

    Code works

--- 2020Fall-Web/Web_Lesson4/Sourcecode/ICP/GitHubUserFinder/script.js
+++ 2020Fall-Web/Web_Lesson4/Sourcecode/ICP/GitHubUserFinder/script.js
@@ -3,22 +3,39 @@ function getGithubInfo(user) {
     // The function should finally return the object(it now contains the response!)
     var xhttp = new XMLHttpRequest();
     xhttp.open("GET", "https://api.github.com/users/"+user, true);
+    xhttp.onload = function () {
+        if (xhttp.readyState === xhttp.DONE) {
+            if (xhttp.status === 200) {
+                console.log(xhttp.responseText);
+                showUser(JSON.parse(xhttp.responseText));
+
+            }
+            else
+                noSuchUser(user);
+        }
+    };
     xhttp.send();
-    return xhttp;
+
 }
 
 function showUser(user) {
     //2. set the contents of the h2 and the two div elements in the div '#profile' with the user content
-    document.getElementsByTagName("h2").innerHTML = user.login;
-    document.getElementsByClassName("avatar").innerHTML= user.avatar_url;
-    document.getElementsByClassName("information").innerHTML= user.html_url;
-    console.log(user.login);
+    document.getElementById('image').src=user.avatar_url;
+    document.getElementById('user_name').innerText=user.name;
+    document.getElementById('user_id').innerText=user.id;
+    document.getElementById('user_url').href=user.url;
+    document.getElementById('user_url').innerText=user.html_url;
+    document.getElementById('user_repository').innerText=user.public_repos;
+    document.getElementById('user_followers').innerText=user.followers;
+    document.getElementById('user_following').innerText=user.following;
+    document.getElementById('user_location').innerText=user.location;
 }
+
 
 function noSuchUser(username) {
     //3. set the elements such that a suitable message is displayed
-    document.getElementsByTagName("h2").innerText = username + "is not found...";
-    console.log("abc");
+    alert("User not found");
+
 }
 
 $(document).ready(function () {
@@ -30,15 +47,10 @@ $(document).ready(function () {
             //reset the text typed in the input
             $(this).val("");
             //get the user's information and store the respsonse
-            response = getGithubInfo(username);
-            //if the response is successful show the user's details
-            if (response.status == 200) {
-                showUser(JSON.parse(response.responseText));
-                //else display suitable message
-            } else {
-                noSuchUser(username);
-            }
+            getGithubInfo(username);
+
+
         }
-        
+
     })
 });
