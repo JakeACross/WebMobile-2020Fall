0f516f0cd83a8cb17535067cde6a03e75c109ee6
JakeACross <jackgw@mail.umkc.edu>
2020-09-28 14:15:48 -0500

    Tmer is done

--- 2020Fall-Web/Web_Lesson5/Sourcecode/ICP/Timer/src/app/app.component.ts
+++ 2020Fall-Web/Web_Lesson5/Sourcecode/ICP/Timer/src/app/app.component.ts
@@ -6,43 +6,41 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-
-
+  // global variable
+  startTimer = 0;
+  // functions
   calcTime(dates) {
     const newYear = new Date('1.1.2020').getTime();
     let date;
+    clearInterval(this.startTimer);
     if (typeof(dates) === 'undefined') {
       date = new Date(newYear).getTime();
     } else {
       date = new Date(dates).getTime();
     }
-    return date;
+    console.log(date);
+    this.updateTimer(date) ; // declaration is below
   }
   updateTimer(date) {
-    const now = new Date().getTime();
-    const distance = date - now;
-    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    this.startTimer = setInterval(() => {
+      const now = new Date().getTime();
+      const distance = date - now;
+      const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+      const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+      const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+      const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+      document.querySelector('.clock-day').innerHTML = '' + days;
+      document.querySelector('.clock-hours').innerHTML = '' + hours;
+      document.querySelector('.clock-minutes').innerHTML = '' + minutes;
+      document.querySelector('.clock-seconds').innerHTML = '' + seconds;
 
-    /*document.querySelector('.clock-day').innerHTML = days;
-    document.querySelector('.clock-hours').innerHTML = hours;
-    document.querySelector('.clock-minutes').innerHTML = minutes;
-    document.querySelector('.clock-seconds').innerHTML = seconds;*/
-
-    if (now >= date) {
-      document.querySelector('.clock-day').innerHTML = 'D';
-      document.querySelector('.clock-hours').innerHTML = 'O';
-      document.querySelector('.clock-minutes').innerHTML = 'N';
-      document.querySelector('.clock-seconds').innerHTML = 'E';
-    }
+      if (now >= date) {
+        clearInterval(this.startTimer);
+        document.querySelector('.clock-day').innerHTML = 'D';
+        document.querySelector('.clock-hours').innerHTML = 'O';
+        document.querySelector('.clock-minutes').innerHTML = 'N';
+        document.querySelector('.clock-seconds').innerHTML = 'E';
+      }
+  }, 1000);
   }
   }
-
-
-
-
-
-
-
