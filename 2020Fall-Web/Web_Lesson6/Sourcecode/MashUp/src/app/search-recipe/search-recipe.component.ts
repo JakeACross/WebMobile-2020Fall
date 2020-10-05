b29a6e2ee565e29d1e08963d4639ffda678bcc7b
JakeACross <jackgw@mail.umkc.edu>
2020-10-05 16:19:18 -0500

    Add commnet and change UI

--- 2020Fall-Web/Web_Lesson6/Sourcecode/MashUp/src/app/search-recipe/search-recipe.component.ts
+++ 2020Fall-Web/Web_Lesson6/Sourcecode/MashUp/src/app/search-recipe/search-recipe.component.ts
@@ -37,11 +37,11 @@ export class SearchRecipeComponent implements OnInit {
     this.placeValue = this.places.nativeElement.value;
 
     if (this.recipeValue !== null) {
-      this._http.get('https://api.edamam.com/search?app_id=00ea7c3e&app_key=f0aa9f9f7e9b21c16250d317054a28f2&from=0&to=3&q='
+      this._http.get('https://api.edamam.com/search?app_id=00ea7c3e&app_key=f0aa9f9f7e9b21c16250d317054a28f2&from=0&to=5&q='
           + this.recipeValue)
           .subscribe((data: any) => {
-              const items = data.hits;
-              for (let i = 0 ; i <= items.length; i++) {
+              const items = data.hits; // accessing recipe (hits[i]->)
+              for (let i = 0 ; i <= items.length; i++) { // iterate through the list which data.hits has and push to recipeList
                   this.recipeList.push(items[i].recipe);
               }
               // console.log(this.recipeList);
@@ -49,11 +49,11 @@ export class SearchRecipeComponent implements OnInit {
     }
 
     if (this.placeValue != null && this.placeValue !== '' && this.recipeValue != null && this.recipeValue !== '') {
-      this._http.get('https://api.foursquare.com/v2/venues/explore?client_id=34YIGNPKOFYNCWWKBRAT541KNYQVDYGDCDILNM5VLPKDXTHR&client_secret=R5FN0SMVEPXER5OFMNYVK3WK12QJRBGTRJWHP3CRV5X0L5GL&v=20180323&limit=5&near=' + this.placeValue + '&query='
+      this._http.get('https://api.foursquare.com/v2/venues/explore?client_id=34YIGNPKOFYNCWWKBRAT541KNYQVDYGDCDILNM5VLPKDXTHR&client_secret=R5FN0SMVEPXER5OFMNYVK3WK12QJRBGTRJWHP3CRV5X0L5GL&v=20180323&limit=9&near=' + this.placeValue + '&query='
           + this.recipeValue)
           .subscribe((data: any) => {
-            const items = data.response.groups[0].items;
-            for (let i = 0 ; i <= items.length; i++) {
+            const items = data.response.groups[0].items; // accessing venue (response->groups[0]->items[i]->)
+            for (let i = 0 ; i <= items.length; i++) { // iterate through the list which items has and push to recipeList
               this.venueList.push(items[i].venue);
             }
             // console.log(this.venueList);
